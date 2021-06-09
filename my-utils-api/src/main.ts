7ca00c311017015dc987fd4858485eb4f7b13988
Janusz Matyjasik <matyjaisk.janusz@gmail.com>
2021-06-09 12:00:06 +0200

    log port

--- my-utils-api/src/main.ts
+++ my-utils-api/src/main.ts
@@ -6,6 +6,8 @@ require('dotenv').config()
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  console.log({port: process.env.PORT});
+  
   await app.listen(process.env.PORT);
 }
 bootstrap();
